4a6fd902fb2c65a5652a8f830be3dc1ec817e1c7
Reza Lotfi <mr.developerx@gmail.com>
2021-12-02 02:21:10 +0330

    DI Modules and providers added

--- app/src/main/java/com/reza/carsproject/di/modules/DataModule.kt
+++ app/src/main/java/com/reza/carsproject/di/modules/DataModule.kt
@@ -1,7 +1,24 @@
 package com.reza.carsproject.di.modules
 
+import com.reza.carsproject.screens.maps.data.MapsApiService
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
+import java.util.concurrent.TimeUnit
 
 val dataModule = module {
+
+    // Provide OkHTTPClient
+    single {
+        OkHttpClient.Builder()
+            .readTimeout(1, TimeUnit.MINUTES)
+            .writeTimeout(1, TimeUnit.MINUTES)
+            .build()
+    }
+
+    // Provide MapsApiService
+    single {
+        MapsApiService.createRetrofit(get())
+    }
+
 
 }
